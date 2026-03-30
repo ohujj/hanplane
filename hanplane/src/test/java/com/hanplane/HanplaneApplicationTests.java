6421ead10e3cba5032f8436e4515851b5c68bf3e
오현우 <ohu1776@gmail.com>
2026-03-30 23:27:51 +0900

    feat: 프로젝트 초기 세팅

---
+++ hanplane/src/test/java/com/hanplane/HanplaneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hanplane;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HanplaneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
