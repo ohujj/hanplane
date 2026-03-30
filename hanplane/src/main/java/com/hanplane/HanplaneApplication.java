a31c73310a51677f9254f15f5cbabba2162a0090
ohujj <114215140+ohujj@users.noreply.github.com>
2026-03-30 23:27:51 +0900

    feat: 프로젝트 초기 세팅

---
+++ hanplane/src/main/java/com/hanplane/HanplaneApplication.java
@@ -0,0 +1,13 @@
+package com.hanplane;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HanplaneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HanplaneApplication.class, args);
+	}
+
+}
