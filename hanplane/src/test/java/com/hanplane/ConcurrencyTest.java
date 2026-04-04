8df4d1ecadc929fe11667c4046fff213259b49e8
User <ohu1776@gmail.com>
2026-04-04 16:23:48 +0900

    [feat] H2 DB -> MySQL로 변경, 동시성 테스트 코드 및 DB Lock 코드 추가 DB Lock 코드로 최종 반영 상태

---
+++ hanplane/src/test/java/com/hanplane/ConcurrencyTest.java
@@ -0,0 +1,86 @@
+package com.hanplane;
+
+import com.hanplane.domain.coupon.entity.Coupon;
+import com.hanplane.domain.coupon.repository.CouponRepository;
+import com.hanplane.domain.coupon.repository.UserCouponRepository;
+import com.hanplane.domain.coupon.service.CouponService;
+import com.hanplane.domain.user.entity.User;
+import com.hanplane.domain.user.repository.UserRepository;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.CountDownLatch;
+
+@SpringBootTest
+public class ConcurrencyTest {
+
+    @Autowired
+    private CouponService couponService;
+    @Autowired
+    private CouponRepository couponRepository;
+    @Autowired
+    private UserCouponRepository userCouponRepository;
+    @Autowired
+    private UserRepository userRepository;
+
+    @Test
+    void 동시에_30명이_쿠폰_발급_요청() throws InterruptedException {
+        // given
+        Coupon coupon = Coupon.builder()
+                .name("100명 제한 쿠폰")
+                .totalQuantity(10)
+                .expiredAt(LocalDateTime.now().plusDays(7))
+                .discountRate(10)
+                .build();
+
+        Coupon savedCoupon = couponRepository.save(coupon);
+
+        List<User> userList = new ArrayList<>();
+        for(int i=1; i<=30; i++) {
+            User user = User.builder()
+                    .email("user" + i + "@test.com")
+                    .password("testUser" + i)
+                    .build();
+            User savedUser = userRepository.save(user);
+            userList.add(savedUser);
+        }
+
+        // when
+        CountDownLatch startLatch = new CountDownLatch(1);
+        CountDownLatch endLatch = new CountDownLatch(30);
+
+        for(User user : userList) {
+            Long userId = user.getId();
+
+            new Thread(() -> {
+                try {
+                    startLatch.await();
+                    couponService.issueCoupon(userId, savedCoupon.getId()); // <- 여기 내부에서 코드 변경
+                } catch (InterruptedException e) {
+                    Thread.currentThread().interrupt();
+                } finally {
+                    endLatch.countDown();
+                }
+            }).start();
+        }
+
+        startLatch.countDown();
+
+        // 100개 스레드 전부 끝날 때까지 대기
+        endLatch.await();
+
+        // then
+        Coupon findCoupon = couponRepository.findById(savedCoupon.getId()).orElseThrow();
+        int issuedQuantity = findCoupon.getIssuedQuantity();
+
+        System.out.println("발급 된 수량 : " + issuedQuantity);
+
+        Assertions.assertThat(issuedQuantity).isEqualTo(10);
+
+    }
+}
