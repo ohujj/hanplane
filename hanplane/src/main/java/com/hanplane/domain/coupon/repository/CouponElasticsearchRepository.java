3f2ba0d9123bb096ea695c60e5e46b001c967836
User <ohu1776@gmail.com>
2026-04-27 21:54:59 +0900

    [feat] 쿠폰 생성 API 추가, 이벤트 리스너 추가(테스트는 아직)

--- hanplane/src/main/java/com/hanplane/domain/coupon/repository/CouponElasticsearchRepository.java
+++ hanplane/src/main/java/com/hanplane/domain/coupon/repository/CouponElasticsearchRepository.java
@@ -7,11 +7,12 @@ import org.springframework.data.elasticsearch.annotations.Query;
 import org.springframework.data.elasticsearch.repository.ElasticsearchRepository;
 
 public interface CouponElasticsearchRepository extends ElasticsearchRepository<CouponDocument, Long> {
+
     Page<CouponDocument> findByName(String keyword, Pageable pageable);
 
 //    @Query("{\"wildcard\": {\"name\": {\"value\": \"*?0*\"}}}")
-@Query("{\"match\": {\"name\": \"?0\"}}")
-Page<CouponDocument> findByNameContaining(String keyword, Pageable pageable);
+    @Query("{\"match\": {\"name\": \"?0\"}}")
+    Page<CouponDocument> findByNameContaining(String keyword, Pageable pageable);
 
 
 
