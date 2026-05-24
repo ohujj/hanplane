483ac82572e8bd61795e55b20c1bdb5b83941d3b
ohujj <114215140+ohujj@users.noreply.github.com>
2026-05-25 01:24:34 +0900

    불 필요한 redis 코드 제거 및 import 제거

--- hanplane/src/main/java/com/hanplane/domain/coupon/service/CouponService.java
+++ hanplane/src/main/java/com/hanplane/domain/coupon/service/CouponService.java
@@ -1,18 +1,8 @@
 package com.hanplane.domain.coupon.service;
 
-import com.hanplane.domain.coupon.dto.CouponListResponse;
-import com.hanplane.global.exception.BusinessException;
-import com.hanplane.global.exception.ErrorCode;
-import com.hanplane.global.jwt.UserPrincipal;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.redisson.api.RLock;
-import org.redisson.api.RedissonClient;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
-
-import java.util.List;
-import java.util.concurrent.TimeUnit;
 
 @Service
 @Slf4j
@@ -21,33 +11,7 @@ public class CouponService {
 
     private final CouponIssueService couponIssueService;
 
-    private final RedissonClient redissonClient;
-
-    private final String couponLockKey = "coupon:lock:";
-
     public void issueCoupon(Long userId, Long couponId) {
-
-        RLock lock = redissonClient.getLock(couponLockKey + couponId);
-
-        try {
-            boolean hasLock = lock.tryLock(30, 3, TimeUnit.SECONDS);
-            if (!hasLock) {
-                throw new BusinessException(ErrorCode.LOCK_TRY_FAIL);
-            }
-
-            couponIssueService.issue(userId, couponId);
-
-        } catch (InterruptedException e) {
-            Thread.currentThread().interrupt();
-        } finally {
-            if(lock.isHeldByCurrentThread()) {
-                lock.unlock();
-            }
-        }
-
-    }
-
-    public void issueCouponWithPessimisticLock(Long userId, Long couponId) {
         couponIssueService.issueWithPessimisticLock(userId, couponId);
     }
 }
